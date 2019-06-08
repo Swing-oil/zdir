7062a653515ca61ff40dfe4f4c7be441e11a6304
Swing-oil <44920669+Swing-oil@users.noreply.github.com>
2019-06-08 16:36:40 +0800

    Update and rename config.simple.php to config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	$siteinfo = array(
-		"title"		=>	"Zdir 实用的目录列表程序",
-		"keywords"	=>	"zdir,h5ai,Directory Lister,Fdscript,目录列表,目录索引",
-		"description"	=>	"Zdir是一款使用PHP开发的目录列表程序，简单实用，免费开源。"
+		"title"		=>	"CPan",
+		"keywords"	=>	"CPan",
+		"description"	=>	"CPan"
 	);
 
 	//需要忽略的目录
